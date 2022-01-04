46fc7be3ce28f655d8b3d78cc19c71eea55a2b39
pponski <pponski@outlook.com>
2022-01-04 17:12:46 +0100

    testing aos

--- components/utils/ProjectCard.js
+++ components/utils/ProjectCard.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
-const ProjectCard = ({imageURL}) => {
+const ProjectCard = ({imageURL,delay}) => {
+
+    useEffect(() => {
+        Aos.init();
+    }, []);
 
     return (
-        <div style={{backgroundImage: `url(/${imageURL})`}} className='h-[350px] rounded-md project-bg flex items-end text-left text-white xl:hover:shadow-xl xl:hover:translate-y-[-10px] xl:hover:odd:rotate-2 xl:hover:even:rotate-[-2deg] duration-500 cursor-default'>
-            <div className='pl-4 pb-8'>
-                <p className='font-bold text-md pb-2'>Tax Maganement</p>
-                <p className='text-xs sm:text-sm pb-6'>Life Tips From Top Ten Adventure Travelers</p>
-                <button className='bg-primary px-8 py-2 rounded-full text-xs lg:text-sm lg:hover:translate-x-2 duration-300'>View Project</button>
+        <div data-aos="fade-up" data-aos-duration='1000' data-aos-delay={delay}>
+            <div style={{backgroundImage: `url(/${imageURL})`}} className='h-[350px] rounded-md project-bg flex items-end text-left text-white xl:hover:shadow-xl xl:hover:even:translate-y-[-20px] xl:hover:odd:translate-y-[20px] xl:hover:odd:rotate-2 xl:hover:even:rotate-[-2deg] duration-500 cursor-default'>
+                <div className='pl-4 pb-8'>
+                    <p className='font-bold text-md lg:text-base pb-2'>Tax Maganement</p>
+                    <p className='text-sm sm:text-md font-light pb-6'>Life Tips From Top Ten Adventure Travelers</p>
+                    <button className='bg-primary px-8 py-2 rounded-full text-xs lg:text-sm lg:hover:translate-x-2 duration-300'>View Project</button>
+                </div>
             </div>
         </div>
     )
--- components/utils/SectionHeading.js
+++ components/utils/SectionHeading.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const SectionHeading = ({title, description}) => {
+
+    useEffect(() => {
+        Aos.init();
+    }, []);
+
     return (
-        <div className='text-white'>
+        <div data-aos="zoom-in" data-aos-duration='1000' data-aos-offset='200' className='text-white'>
             <p className='text-3xl font-bold pb-3 sm:text-4xl sm:pb-5'>{title}</p>
             <p className='text-xs px-8 font-light leading-5 sm:text-base sm:leading-7'>{description}</p>
         </div>
